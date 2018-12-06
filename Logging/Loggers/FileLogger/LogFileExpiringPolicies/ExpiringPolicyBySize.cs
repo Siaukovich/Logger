5f8b15b48552d2279cdf9749817252093a41417e
Kiryl Siaukovich <k.sevkovich@gmail.com>
2018-12-06 14:12:11 +0300

    ExpiringPolicies by time and size were created.

---
+++ Logging/Loggers/FileLogger/LogFileExpiringPolicies/ExpiringPolicyBySize.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using Logging.Base;
+
+namespace Logging.Loggers.FileLogger.LogFileExpiringPolicies
+{
+    internal class ExpiringPolicyBySize : ILogFileExpiringPolicy
+    {
+        private readonly int _maxSizeKB;
+
+        public ExpiringPolicyBySize(int maxSizeKB)
+        {
+            if (maxSizeKB <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(maxSizeKB));
+            }
+
+            _maxSizeKB = maxSizeKB;
+        }
+
+        public bool IsExpired(string logFilePath)
+        {
+            if (string.IsNullOrEmpty(logFilePath))
+            {
+                throw new ArgumentException($"{nameof(logFilePath)} must not be empty.", nameof(logFilePath));
+            }
+
+            if (!File.Exists(logFilePath))
+            {
+                throw new FileNotFoundException($"{nameof(logFilePath)} was not found.", nameof(logFilePath));
+            }
+
+            var file = new FileInfo(logFilePath);
+            var fileLengthBytes = file.Length;
+            var BYTES_IN_KB = 1024;
+
+            return fileLengthBytes * BYTES_IN_KB >= _maxSizeKB;
+        }
+    }
+}
---
+++ Logging/Loggers/FileLogger/LogFileExpiringPolicies/ExpiringPolicyByTime.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using Logging.Base;
+
+namespace Logging.Loggers.FileLogger.LogFileExpiringPolicies
+{
+    internal class ExpiringPolicyByTime : ILogFileExpiringPolicy
+    {
+        private readonly int _maxFileLifetimeSeconds;
+
+        public ExpiringPolicyByTime(int maxFileLifetimeSeconds)
+        {
+            if (maxFileLifetimeSeconds <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(maxFileLifetimeSeconds));
+            }
+
+            _maxFileLifetimeSeconds = maxFileLifetimeSeconds;
+        }
+
+        public bool IsExpired(string logFilePath)
+        {
+            if (string.IsNullOrEmpty(logFilePath))
+            {
+                throw new ArgumentException($"{nameof(logFilePath)} must not be empty.", nameof(logFilePath));
+            }
+
+            if (!File.Exists(logFilePath))
+            {
+                throw new FileNotFoundException($"{nameof(logFilePath)} was not found.", nameof(logFilePath));
+            }
+
+            var file = new FileInfo(logFilePath);
+            var creationTime = file.CreationTime;
+            var lifetime = (DateTime.Now - creationTime).Seconds;
+
+            return lifetime >= _maxFileLifetimeSeconds;
+        }
+    }
+}
