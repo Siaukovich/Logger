12dd2f84ad7961f30b243ee975caefe158925504
Kiryl Siaukovich <k.sevkovich@gmail.com>
2018-12-06 14:13:31 +0300

    Fake factory was created.

--- Logging/LoggerFactory.cs
+++ Logging/LoggerFactory.cs
@@ -1,16 +1,29 @@
-﻿using Logging.Base;
+﻿using System.Collections.Generic;
+
+using Logging.Base;
+using Logging.Loggers;
+using Logging.Loggers.FileLogger;
+using Logging.Loggers.FileLogger.LogFileExpiringPolicies;
 
 namespace Logging
 {
-    using Logging.Loggers;
-
     public static class LoggerFactory
     {
-        public static AbstractLogger GetFileLogger(LogLevel level, string messageLayout, ILogFileExpiringPolicy policy, string filepath)
+        public static ILogger GetFileLogger(LogLevel level, string messageLayout, ILogFileExpiringPolicy policy, string filepath)
         {
-            return new FileLogger(level, messageLayout, policy, filepath);
+            // In the best scenario, factory should read all targets from config file,
+            // but I had no time for it, so let me just hardcode loggers.
+
+            const string layout = "[${logLevel}] ${datetime}: ${message} | ${ex} ${newline}";
+            List<AbstractLogger> targets = new List<AbstractLogger>
+            {
+                new ConsoleLogger(LogLevel.Debug, layout),
+                new FileLogger(LogLevel.Error, layout, new ExpiringPolicyBySize(1024), "${basedir}/logs")
+            };
+
+            ILogger wrapper = new LoggersWrapper(targets);
+
+            return wrapper;
         }
-
-
     }
 }
