cad7affc38be4e64f4e430e3b1f8ea4c8f4abf71
Kiryl Siaukovich <k.sevkovich@gmail.com>
2018-12-06 03:04:49 +0300

    Logger factory draft was created.

---
+++ Logging/LoggerFactory.cs
@@ -0,0 +1,16 @@
+﻿using Logging.Base;
+
+namespace Logging
+{
+    using Logging.Loggers;
+
+    public static class LoggerFactory
+    {
+        public static AbstractLogger GetFileLogger(LogLevel level, string messageLayout, ILogFileExpiringPolicy policy, string filepath)
+        {
+            return new FileLogger(level, messageLayout, policy, filepath);
+        }
+
+
+    }
+}
