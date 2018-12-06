5ddf113f9fd09d259dbbd9585e6c4336358c8e44
Kiryl Siaukovich <k.sevkovich@gmail.com>
2018-12-06 03:03:45 +0300

    Draft for console logger was created.

---
+++ Logging/Loggers/ConsoleLogger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using Logging.Base;
+
+namespace Logging.Loggers
+{
+    public class ConsoleLogger : AbstractLogger
+    {
+        private readonly object _syncObject = new object();
+
+        public ConsoleLogger(LogLevel level, string logMessageLayout)
+            : base(level, logMessageLayout)
+        {
+        }
+
+        protected override void Write(string logEntry)
+        {
+            lock (_syncObject)
+            {
+                Console.WriteLine(logEntry);
+            }
+        }
+    }
+}
